df9208ed8e348ba4a2bd4b7572832d73267b36c5
JoongChangYang <didwndckd@gmail.com>
2020-07-13 22:43:09 +0900

    add: Study.swift

---
+++ Fastcampus/Fastcampus/Models/Study.swift
@@ -0,0 +1,34 @@
+//
+//  Study.swift
+//  Fastcampus
+//
+//  Created by 양중창 on 2020/07/13.
+//  Copyright © 2020 Kira. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+
+struct Study: Codable {
+  
+  let id: Int
+  let title: String
+  let date: Date
+  let Rule: String
+  var userIDs: [String]
+  var qnaIDs: [String]
+  var inProcess: ProcessStatus
+  
+  enum ProcessStatus: Int, Codable {
+    case wait
+    case inProcess
+    case finished
+  }
+  
+  
+  func test() {
+    
+  }
+  
+}
