e21f1362ff2bbf2f6af0fadf289c20db6de58a63
JoongChangYang <didwndckd@gmail.com>
2020-07-13 23:31:14 +0900

    add: StudyFrameView, TimerView

---
+++ Fastcampus/Fastcampus/Views/Study/StudyFrameView.swift
@@ -0,0 +1,13 @@
+//
+//  StudyFrameView.swift
+//  Fastcampus
+//
+//  Created by 양중창 on 2020/07/13.
+//  Copyright © 2020 Kira. All rights reserved.
+//
+
+import UIKit
+
+class StudyFrameView: View {
+
+}
---
+++ Fastcampus/Fastcampus/Views/Study/TimerView.swift
@@ -0,0 +1,13 @@
+//
+//  TimerView.swift
+//  Fastcampus
+//
+//  Created by 양중창 on 2020/07/13.
+//  Copyright © 2020 Kira. All rights reserved.
+//
+
+import UIKit
+
+class TimerView: View {
+
+}
