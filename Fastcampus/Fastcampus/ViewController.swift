6e2e1a2ea7d9447e04c5ff808552f7ff287e3e4f
dldbdjq <dldbdjq@gmail.com>
2020-07-13 11:29:50 +0900

    ADD: Project
     - 프로젝트 생성

---
+++ Fastcampus/Fastcampus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fastcampus
+//
+//  Created by Lee on 2020/07/13.
+//  Copyright © 2020 Kira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
