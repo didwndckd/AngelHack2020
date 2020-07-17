808496f4403176c43a705545d33503fb65937bfd
JoongChangYang <didwndckd@gmail.com>
2020-07-18 06:08:10 +0900

    ADD: DateExtension.swift, CircleView.swift, StudyInformationView.swift

---
+++ Fastcampus/Fastcampus/Extension/DateExtension.swift
@@ -0,0 +1,9 @@
+//
+//  DateExtension.swift
+//  Fastcampus
+//
+//  Created by 양중창 on 2020/07/18.
+//  Copyright © 2020 Kira. All rights reserved.
+//
+
+import Foundation
---
+++ Fastcampus/Fastcampus/Views/Study/Waiting/StudyInformationView.swift
@@ -0,0 +1,21 @@
+//
+//  StudyInformationView.swift
+//  Fastcampus
+//
+//  Created by 양중창 on 2020/07/18.
+//  Copyright © 2020 Kira. All rights reserved.
+//
+
+import UIKit
+
+class StudyInformationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
