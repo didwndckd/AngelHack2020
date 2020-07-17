25c5e2bfe4707e9860ba8025abd85be6b700bbba
JoongChangYang <didwndckd@gmail.com>
2020-07-17 19:37:08 +0900

    ADD: QustionCell.swift

---
+++ Fastcampus/Fastcampus/Cells/QuestionCell.swift
@@ -0,0 +1,21 @@
+//
+//  QuestionCell.swift
+//  Fastcampus
+//
+//  Created by 양중창 on 2020/07/17.
+//  Copyright © 2020 Kira. All rights reserved.
+//
+
+import UIKit
+
+class QuestionCell: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
