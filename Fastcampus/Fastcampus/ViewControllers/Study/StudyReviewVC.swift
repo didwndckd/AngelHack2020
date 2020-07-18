54bbbd230649c02750ea291b4076cf28a65f4ee8
JoongChangYang <didwndckd@gmail.com>
2020-07-19 04:09:55 +0900

    Add StudyReviewVC.swift QuestionButton.swift StudyReviewView.swift images

---
+++ Fastcampus/Fastcampus/ViewControllers/Study/StudyReviewVC.swift
@@ -0,0 +1,30 @@
+//
+//  StudyReViewVC.swift
+//  Fastcampus
+//
+//  Created by 양중창 on 2020/07/19.
+//  Copyright © 2020 Kira. All rights reserved.
+//
+
+import UIKit
+
+class StudyReViewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Fastcampus/Fastcampus/Views/Study/InProcess/QuestionButton.swift
@@ -0,0 +1,21 @@
+//
+//  QuestionButton.swift
+//  Fastcampus
+//
+//  Created by 양중창 on 2020/07/18.
+//  Copyright © 2020 Kira. All rights reserved.
+//
+
+import UIKit
+
+class QuestionButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
