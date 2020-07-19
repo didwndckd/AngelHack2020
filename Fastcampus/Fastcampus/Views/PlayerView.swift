60a80e8b163f1a60debe5d25f4e428b31bcb88ce
JoongChangYang <didwndckd@gmail.com>
2020-07-19 15:14:32 +0900

    ADD: PlayerViewController.swift, PlayerView.swift

---
+++ Fastcampus/Fastcampus/Views/PlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerView.swift
+//  Fastcampus
+//
+//  Created by 양중창 on 2020/07/19.
+//  Copyright © 2020 Kira. All rights reserved.
+//
+
+import UIKit
+
+class PlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
